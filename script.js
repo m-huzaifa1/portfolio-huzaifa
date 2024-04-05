84851daae8b7d8d2f885da528cd9ac8f04409e3d
m-huzaifa1 <huzaifa3208@gmail.com>
2024-04-06 02:14:52 +0500

    second commit

---
+++ script.js
@@ -0,0 +1,28 @@
+let darkModeEnabled = localStorage.getItem('darkModeEnabled') === 'true';
+
+const enableDarkMode = () => {
+    darkModeEnabled = true;
+    document.body.classList.add('dark-mode');
+    localStorage.setItem('darkModeEnabled', 'true');
+}
+
+const disableDarkMode = () => {
+    darkModeEnabled = false;
+    document.body.classList.remove('dark-mode');
+    localStorage.setItem('darkModeEnabled', 'false');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    darkModeButton.checked = darkModeEnabled;
+    document.body.classList.toggle('dark-mode', darkModeEnabled);
+});
+
+const darkModeButton = document.getElementById('darkModeButton');
+
+darkModeButton.addEventListener('change', () => {
+    if (darkModeButton.checked) {
+        enableDarkMode();
+    } else {
+        disableDarkMode();
+    }
+});
